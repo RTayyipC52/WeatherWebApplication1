f175670c04a8a43a3c65d69ee7911ba00e89df36
RTayyipC52 <tayyipcicek52@gmail.com>
2021-11-10 21:25:00 +0300

    Add project files.

---
+++ WeatherWebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using WeatherWebApplication1.Models;
+
+namespace WeatherWebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HttpClient client = new HttpClient();
+
+            var responseMessage = client.GetAsync("https://www.metaweather.com/api/location/44418/2013/4/27/").Result;
+            List<WeatherForecast> weathers = null;
+            if (responseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                weathers = JsonConvert.DeserializeObject<List<WeatherForecast>>(responseMessage.Content.ReadAsStringAsync().Result);
+            }
+
+            return View(weathers);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WeatherWebApplication1/Models/WeatherForecast.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherWebApplication1.Models
+{
+    public class WeatherForecast
+    {
+        [DisplayName("Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Weather State Name")]
+        public string Weather_state_name { get; set; }
+
+        [DisplayName("Weather State Abbr")]
+        public string Weather_state_abbr { get; set; }
+
+        [DisplayName("Wind Direction Compass")]
+        public string Wind_direction_compass { get; set; }
+
+        [DisplayName("Created")]
+        public DateTime Created { get; set; }
+
+        [DisplayName("Applicable Date")]
+        public string Applicable_date { get; set; }
+
+        [DisplayName("Min Temp")]
+        public float? Min_temp { get; set; }
+
+        [DisplayName("Max Temp")]
+        public float? Max_temp { get; set; }
+
+        [DisplayName("The Temp")]
+        public float? The_temp { get; set; }
+
+        [DisplayName("Wind Speed")]
+        public float? Wind_speed { get; set; }
+
+        [DisplayName("Wind Direction")]
+        public float Wind_direction { get; set; }
+
+        [DisplayName("Air Pressure")]
+        public float? Air_pressure { get; set; }
+
+        [DisplayName("Humidity")]
+        public int? Humidity { get; set; }
+
+        [DisplayName("Visibility")]
+        public float? Visibility { get; set; }
+
+        [DisplayName("Predictability")]
+        public int Predictability { get; set; }
+    }
+    
+}
